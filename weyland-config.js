9386ab89b47eb248fc257b67a808d2d7d61f1921
EisenbergEffect <rob@bluespire.com>
2013-06-22 14:30:02 -0400

    Making dsl output consistent with config structure.
    -Trying to remove idea.

--- weyland-config.js
+++ weyland-config.js
@@ -1,5 +1,6 @@
 exports.config = function(weyland) {
     weyland.build('main')
+        .workingDirectory('C:/Users/Rob/Documents/GitHub/Durandal/platforms/Microsoft.NET/Samples/Durandal.Samples/App')
         .task.jshint({
             glob:'**/*.js',
             stopOnErrors:false,
