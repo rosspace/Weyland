5c0428dc21a9bfa9b856e2ef425e7956bc17dda8
EisenbergEffect <rob@bluespire.com>
2013-06-22 20:53:14 -0400

    Enabling task includes, excludes and static files.

--- weyland-config.js
+++ weyland-config.js
@@ -2,15 +2,15 @@ exports.config = function(weyland) {
     weyland.build('main')
         .workingDirectory('C:/Users/Rob/Documents/GitHub/Durandal/platforms/Microsoft.NET/Samples/Durandal.Samples/App')
         .task.jshint({
-            glob:'**/*.js',
+            include:'**/*.js',
             stopOnErrors:false,
             options:undefined,
             globals:undefined
         })
         .task.uglifyjs({
-            glob:'**/*.js'
+            include:'**/*.js'
         })
         .task.rjs({
-            glob:'**/*.{js,html}'
+            include:'**/*.{js,html}'
         });
 }
