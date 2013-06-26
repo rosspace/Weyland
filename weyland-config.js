54cc7d2341962a6d7e5d35ce81e845af1d51e4e1
EisenbergEffect <rob@bluespire.com>
2013-06-26 01:16:31 -0400

    Starting work on r.js task.

--- weyland-config.js
+++ weyland-config.js
@@ -8,6 +8,31 @@ exports.config = function(weyland) {
             include:'App/**/*.js'
         })
         .task.rjs({
-            include:'App/**/*.{js,html}'
+            include:'App/**/*.{js,html}',
+            viewPlugin:'text',
+            viewPluginExtension:'.html',
+            rjs:{
+                name:'../Scripts/almond-custom', //for require use the build's name
+                baseUrl : 'App',
+                mainConfigFile:'App/main', //not needed for require
+                wrap:true, //not needed for require
+                paths : {
+                    'text': '../Scripts/text',
+                    'durandal': '../Scripts/durandal',
+                    'plugins': '../Scripts/durandal/plugins',
+                    'transitions': '../Scripts/durandal/transitions',
+                    'knockout': '../Scripts/knockout-2.2.1',
+                    'bootstrap': '../Scripts/bootstrap',
+                    'jquery': '../Scripts/jquery-1.9.1'
+                },
+                inlineText: true,
+                optimize : 'none',
+                pragmas: {
+                    build: true
+                },
+                stubModules : ['text'],
+                keepBuildDir: true,
+                out:'App/main-built.js'
+            }
         });
 }
