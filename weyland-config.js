ed71157705e88d98dc1cdc1a916e2c5a293bfbfa
EisenbergEffect <rob@bluespire.com>
2013-08-14 11:52:23 -0400

    Removing the hardcoded working directory.

--- weyland-config.js
+++ weyland-config.js
@@ -1,6 +1,5 @@
 exports.config = function(weyland) {
     weyland.build('main')
-        .workingDirectory('C:/Users/Rob/Documents/GitHub/Durandal/platforms/Microsoft.NET/Samples/Durandal.Samples')
         .task.jshint({
             include:'App/**/*.js'
         })
